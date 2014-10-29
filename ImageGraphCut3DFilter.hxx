1eeadf055dc6502d9ec81ba442476daddee752ce
Thomas Fitze <fitz@zhaw.ch>
2014-10-29 07:42:28 +0100

    initialized node image

--- ImageGraphCut3DFilter.hxx
+++ ImageGraphCut3DFilter.hxx
@@ -16,10 +16,19 @@ namespace itk {
     template<typename TImage, typename TForeground, typename TBackground, typename TOutput>
     void ImageGraphCut3DFilter<TImage, TForeground, TBackground, TOutput>
     ::GenerateData() {
-        // get input
+        // get all images
         const InputImageType *inputImage = GetInputImage();
         const ForegroundImageType *foregroundImage = GetForegroundImage();
         const BackgroundImageType *backgroundImage = GetBackgroundImage();
+        OutputImageType *outputImage = this->GetOutput();
+
+        // init node image
+        NodeImageType::Pointer nodeImage = NodeImageType::New();
+        nodeImage->SetRegions(inputImage->GetLargestPossibleRegion());
+        nodeImage->Allocate();
+        nodeImage->SetBufferedRegion(nodeImage->GetLargestPossibleRegion());
+        nodeImage->FillBuffer(NULL);
+
     }
 }
 
