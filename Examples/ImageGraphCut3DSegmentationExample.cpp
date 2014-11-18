d5ef65ace28a806ae14dba3d87ae448f21f16b29
Thomas Fitze <fitz@zhaw.ch>
2014-11-18 10:47:07 +0100

    fixed imports

--- Examples/ImageGraphCut3DSegmentationExample.cpp
+++ Examples/ImageGraphCut3DSegmentationExample.cpp
@@ -1,35 +1,9 @@
-/*
-Author: Yves Pauchard, yves.pauchard@zhaw.ch
-Date: Nov 5, 2013
-Modified: Jan 21+22, 2014: Now exposes new option of the graphCut class.
-
-Description: Adaptation of the 2D RGB version by David Doria to 3D single value images.
-
-
-Based on ImageGraphCutSegmentationExample.cpp:
-Copyright (C) 2012 David Doria, daviddoria@gmail.com
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "ImageGraphCut3DFilter.h"
 
 
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkImageRegionConstIteratorWithIndex.h"
 
 
 /** Adapted version of GetPixelsWithValue from ITKHelpers */
