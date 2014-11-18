e72170c00e17fbcafc3a976c1b5870be4db34c9a
Thomas Fitze <fitz@zhaw.ch>
2014-11-18 12:55:32 +0100

    added license header

--- MaxFlowGraphBoost.hxx
+++ MaxFlowGraphBoost.hxx
@@ -1,4 +1,20 @@
-#ifndef __MaxFlowGraphBoost_hxx_
+/* * *
+ * Image GraphCut 3D Segmentation
+ * Copyright (C) 2014 Visual Computing Lab (visualcomputinglab@dornbirn.zhaw.ch)
+ * Institute of Applied Information Technology, Zurich University of Applied Sciences
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * * */
+
+ #ifndef __MaxFlowGraphBoost_hxx_
 #define __MaxFlowGraphBoost_hxx_
 
 // boost
