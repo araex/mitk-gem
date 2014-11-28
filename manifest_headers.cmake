25189bb852b402d8240c69342565e9a34580c9b1
Thomas Fitze <fitz@zhaw.ch>
2014-11-28 08:39:26 +0100

    changed plugin vendor

--- manifest_headers.cmake
+++ manifest_headers.cmake
@@ -1,5 +1,5 @@
 set(Plugin-Name "Padding")
 set(Plugin-Version "0.1")
-set(Plugin-Vendor "DKFZ, Medical and Biological Informatics")
+set(Plugin-Vendor "ZHAW")
 set(Plugin-ContactAddress "")
 set(Require-Plugin org.mitk.gui.qt.common)
