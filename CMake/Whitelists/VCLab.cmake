fcdd19cc053049c01a5870b1275cb2a5bedcc967
Thomas Fitze <fitz@zhaw.ch>
2015-09-03 10:44:57 +0200

    added whitelist on super build level

---
+++ CMake/Whitelists/VCLab.cmake
@@ -0,0 +1,49 @@
+set(enabled_modules
+  AlgorithmsExt
+  AppUtil
+  Core
+  ContourModel
+  CppMicroServices
+  DataTypesExt
+  DICOMReader
+  GraphAlgorithms
+  ImageExtraction
+  ImageStatistics
+  LegacyAdaptors
+  LegacyGL
+  MapperExt
+  Multilabel
+  Overlays
+  PlanarFigure
+  QmlItems
+  QtWidgets
+  QtWidgetsExt
+  RigidRegistration
+  RigidRegistrationUI
+  SceneSerialization
+  SceneSerializationBase
+  Segmentation
+  SegmentationUI
+  SurfaceInterpolation
+)
+
+set(enabled_plugins
+  "org.blueberry.core.commands"
+  "org.blueberry.core.expressions"
+  "org.blueberry.core.runtime"
+  "org.blueberry.ui.qt"
+  "org.blueberry.ui.qt.help"
+  "org.mitk.core.ext"
+  "org.mitk.core.services"
+  "org.mitk.gui.common"
+  "org.mitk.gui.qt.application"
+  "org.mitk.gui.qt.common"
+  "org.mitk.gui.qt.common.legacy"
+  "org.mitk.gui.qt.extapplication"
+  "org.mitk.gui.qt.ext"
+  "org.mitk.gui.qt.stdmultiwidgeteditor"
+  "org.mitk.gui.qt.datamanager"
+  "org.mitk.gui.qt.segmentation"
+  "org.mitk.gui.qt.registration"
+  "org.mitk.gui.qt.imagecropper"
+)
