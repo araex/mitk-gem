273eb1c856b0638870f508596fbf2f2e16bdc595
Thomas Fitze <fitz@zhaw.ch>
2015-10-05 14:25:44 +0200

    QT5 activator fix

--- src/internal/ch_zhaw_padding_Activator.h
+++ src/internal/ch_zhaw_padding_Activator.h
@@ -26,6 +26,9 @@ class ch_zhaw_padding_Activator :
   public QObject, public ctkPluginActivator
 {
   Q_OBJECT
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    Q_PLUGIN_METADATA(IID "ch_zhaw_padding")
+#endif
   Q_INTERFACES(ctkPluginActivator)
 
 public:
