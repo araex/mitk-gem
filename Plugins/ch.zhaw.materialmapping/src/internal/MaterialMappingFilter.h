b995bf83bf57877cbf265189705819ef55efcd02
Thomas Fitze <fitze.thomas@gmail.com>
2016-09-17 09:15:27 +0200

    Codestyle

--- Plugins/ch.zhaw.materialmapping/src/internal/MaterialMappingFilter.h
+++ Plugins/ch.zhaw.materialmapping/src/internal/MaterialMappingFilter.h
@@ -41,79 +41,94 @@
  *
  * The mapping is functionally equivalent to assignElasticModulus.cc 26.11.15 (v3).
  */
-class MaterialMappingFilter : public mitk::UnstructuredGridToUnstructuredGridFilter {
+class MaterialMappingFilter : public mitk::UnstructuredGridToUnstructuredGridFilter
+{
 public:
-    enum class Method {
-        Old, // as originally published
-        New // modified and improved
-    };
+	enum class Method
+	{
+		Old, // as originally published
+		New // modified and improved
+	};
 
-    mitkClassMacro(MaterialMappingFilter, UnstructuredGridToUnstructuredGridFilter)
-    itkFactorylessNewMacro(Self)
-    itkCloneMacro(Self)
+	mitkClassMacro(MaterialMappingFilter, UnstructuredGridToUnstructuredGridFilter)
 
-    void SetMethod(Method _m){
-        m_Method = _m;
-    }
+	itkFactorylessNewMacro(Self)
 
-    void SetIntensityImage(mitk::Image::Pointer _p) {
-        m_IntensityImage = _p;
-    }
+	itkCloneMacro(Self)
 
-    void SetDensityFunctor(BoneDensityFunctor &&_f) {
-        m_BoneDensityFunctor = _f;
-    }
+	void SetMethod(Method _m)
+	{
+		m_Method = _m;
+	}
 
-    void SetPowerLawFunctor(PowerLawFunctor &&_f) {
-        m_PowerLawFunctor = _f;
-    }
+	void SetIntensityImage(mitk::Image::Pointer _p)
+	{
+		m_IntensityImage = _p;
+	}
 
-    void SetDoPeelStep(bool _b) {
-        m_DoPeelStep = _b;
-    }
+	void SetDensityFunctor(BoneDensityFunctor&& _f)
+	{
+		m_BoneDensityFunctor = _f;
+	}
 
-    void SetNumberOfExtendImageSteps(unsigned int _i) {
-        m_NumberOfExtendImageSteps = _i;
-    }
+	void SetPowerLawFunctor(PowerLawFunctor&& _f)
+	{
+		m_PowerLawFunctor = _f;
+	}
 
-    void SetMinElementValue(float _f) {
-        m_MinimumElementValue = _f;
-    }
+	void SetDoPeelStep(bool _b)
+	{
+		m_DoPeelStep = _b;
+	}
 
-    void SetIntermediateResultOutputDirectory(std::string _d){
-        m_VerboseOutput = true;
-        m_VerboseOutputDirectory = _d;
-    }
+	void SetNumberOfExtendImageSteps(unsigned int _i)
+	{
+		m_NumberOfExtendImageSteps = _i;
+	}
 
-    virtual void GenerateData() override;
+	void SetMinElementValue(float _f)
+	{
+		m_MinimumElementValue = _f;
+	}
+
+	void SetIntermediateResultOutputDirectory(std::string _d)
+	{
+		m_VerboseOutput = true;
+		m_VerboseOutputDirectory = _d;
+	}
+
+	virtual void GenerateData() override;
 
 protected:
-    using VtkImage = vtkSmartPointer<vtkImageData>;
-    using VtkStencil = vtkSmartPointer<vtkImageStencil>;
-    using VtkUGrid = vtkSmartPointer<vtkUnstructuredGridBase>;
-    using VtkDoubleArray = vtkSmartPointer<vtkDoubleArray>;
+	using VtkImage = vtkSmartPointer<vtkImageData>;
+	using VtkStencil = vtkSmartPointer<vtkImageStencil>;
+	using VtkUGrid = vtkSmartPointer<vtkUnstructuredGridBase>;
+	using VtkDoubleArray = vtkSmartPointer<vtkDoubleArray>;
 
-    MaterialMappingFilter();
-    virtual ~MaterialMappingFilter() { };
+	MaterialMappingFilter();
 
-    VtkUGrid extractSurface(const VtkUGrid) const;
-    VtkImage extractVOI(const VtkImage, const VtkUGrid) const;
-    VtkImage createStencil(const VtkUGrid, const VtkImage) const;
-    VtkImage createPeeledMask(const VtkImage _img, const VtkImage _mask);
-    void inplaceExtendImage(VtkImage _img, VtkImage _mask, bool _maxVal); // weighted average in neighborhood, performed in place
-    void inplaceExtendImageOld(VtkImage _img, VtkImage _mask, bool _maxVal);
-    void inplaceApplyFunctorsToImage(VtkImage _img);
-    VtkDoubleArray interpolateToNodes(const VtkUGrid, const VtkImage, std::string _name, double _minElem) const; // "interpolateToNodes". evaluates both functors for each vertex of the mesh
-    VtkDoubleArray nodesToElements(const VtkUGrid, VtkDoubleArray _nodeData, std::string _name) const;
+	virtual ~MaterialMappingFilter()
+	{
+	};
 
-    mitk::Image::Pointer m_IntensityImage;
-    BoneDensityFunctor m_BoneDensityFunctor;
-    PowerLawFunctor m_PowerLawFunctor;
-    bool m_DoPeelStep = true, m_VerboseOutput;
-    std::string m_VerboseOutputDirectory;
-    float m_MinimumElementValue = 0.0;
-    unsigned int m_NumberOfExtendImageSteps = 3;
-    Method m_Method;
+	VtkUGrid extractSurface(const VtkUGrid) const;
+	VtkImage extractVOI(const VtkImage, const VtkUGrid) const;
+	VtkImage createStencil(const VtkUGrid, const VtkImage) const;
+	VtkImage createPeeledMask(const VtkImage _img, const VtkImage _mask);
+	void inplaceExtendImage(VtkImage _img, VtkImage _mask, bool _maxVal); // weighted average in neighborhood, performed in place
+	void inplaceExtendImageOld(VtkImage _img, VtkImage _mask, bool _maxVal);
+	void inplaceApplyFunctorsToImage(VtkImage _img);
+	VtkDoubleArray interpolateToNodes(const VtkUGrid, const VtkImage, std::string _name, double _minElem) const; // "interpolateToNodes". evaluates both functors for each vertex of the mesh
+	VtkDoubleArray nodesToElements(const VtkUGrid, VtkDoubleArray _nodeData, std::string _name) const;
 
-    void writeMetaImageToVerboseOut(const std::string filename, vtkSmartPointer<vtkImageData> image);
+	mitk::Image::Pointer m_IntensityImage;
+	BoneDensityFunctor m_BoneDensityFunctor;
+	PowerLawFunctor m_PowerLawFunctor;
+	bool m_DoPeelStep = true, m_VerboseOutput;
+	std::string m_VerboseOutputDirectory;
+	float m_MinimumElementValue = 0.0;
+	unsigned int m_NumberOfExtendImageSteps = 3;
+	Method m_Method;
+
+	void writeMetaImageToVerboseOut(const std::string filename, vtkSmartPointer<vtkImageData> image);
 };
