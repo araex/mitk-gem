59fb3454250f316d26ba1dfb6e1f7967ba3bde06
François Girinon <Celli119@users.noreply.github.com>
2017-05-01 12:09:08 +0200

    Multi label grid cut (#38)
    * required c++ 11 flag to use "using" keyword
    
    * include the GridCut directory
    
    * start creating a pipeline for multi-label segmentation
    
    * working version of multi-labels gridCut
    
    * change random to not random
    
    * change max number of cycles
    
    * change float weight type to unsigned char to save RAM

---
+++ Plugins/ch.zhaw.graphcut/src/internal/lib/GraphCut3D/ImageMultiLabelGridCutFilter.h
@@ -0,0 +1,68 @@
+/**
+ *  Image GraphCut 3D Segmentation
+ *
+ *  Copyright (c) 2016, Zurich University of Applied Sciences, School of Engineering, T. Fitze, Y. Pauchard
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved.
+ */
+
+#ifndef __ImageMultiLabelGridCutFilter_h_
+#define __ImageMultiLabelGridCutFilter_h_
+
+#include "ImageMultiLabelGraphCut3DFilter.h"
+#include "lib/gridcut/examples/include/AlphaExpansion/AlphaExpansion_3D_6C_MT.h"
+
+namespace itk{
+
+template<typename TInput, typename TMultiLabel, typename TOutput>
+class ImageMultiLabelGridCutFilter : public ImageMultiLabelGraphCut3DFilter<TInput, TMultiLabel, TOutput>{
+public:
+	// ITK related defaults
+	typedef ImageMultiLabelGridCutFilter Self;
+	typedef ImageMultiLabelGraphCut3DFilter<TInput, TMultiLabel, TOutput> SuperClass;
+	typedef SmartPointer<Self> Pointer;
+	typedef SmartPointer<const Self> ConstPointer;
+	itkNewMacro(Self);
+
+	itkTypeMacro(ImageMultiLabelGridCutFilter, ImageMultiLabelGridCutFilter);
+
+    typedef typename SuperClass::InputImageType InputImageType;
+
+    typedef typename SuperClass::MultiLabelImageType MultiLabelImageType;
+    typedef typename SuperClass::OutputImageType OutputImageType;
+    typedef typename SuperClass::IndexContainerType IndexContainerType;     // container for sinks / sources
+    typedef typename SuperClass::WeightType WeightType;
+
+    typedef typename SuperClass::ImageContainer ImageContainer;
+    typedef typename std::vector< std::vector<WeightType > > CapacityType;
+    typedef AlphaExpansion_3D_6C_MT<typename TMultiLabel::PixelType, WeightType, WeightType> GraphType;
+
+	virtual void FillGraph(const ImageContainer, ProgressReporter &progress) override;
+    virtual void SolveGraph() override {
+        m_Graph->perform(50);
+    }
+	virtual void CutGraph(ImageContainer, ProgressReporter &progress) override;
+
+
+protected:
+
+	ImageMultiLabelGridCutFilter();
+    virtual ~ImageMultiLabelGridCutFilter();
+
+    std::vector<unsigned int> mLabelIndex;
+    GraphType* m_Graph;
+
+private:
+	ImageMultiLabelGridCutFilter(const Self &); // intentionally not implemented
+	void operator=(const Self &); // intentionally not implemented
+};
+} // namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+
+#include "ImageMultiLabelGridCutFilter.hxx"
+
+#endif
+
+#endif //__ImageMultiLabelGridCutFilter_h_
